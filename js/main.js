afb56016c7d518a06b8347878e930924cf9eb951
Vladimir <vaskorohod@gmail.com>
2019-10-21 22:38:00 +0200

    Add before-after block in index.html, slider plugin, maskedinput plugin

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(window).load(function() {
+  $(".before-after").twentytwenty({
+    before_label: 'Без скинали>', // Set a custom before label
+    after_label: 'Со скинали', // Set a custom after label
+  });
+  $(".before-slider").slick({
+     draggable: false,
+     dots: true,
+     dotsClass: 'before-slider__dots',
+     prevArrow: $('.arrow-left'),
+     nextArrow: $('.arrow-right')
+  });
+  $('.menu-button').on('click', function(){
+    $('.menu').toggleClass('menu_active');
+  });
+  // Select setting
+  $('.select_checked').on('click', function(){
+    $('.select__dropdown').toggleClass('select__dropdown_open')
+  });
+  $('.select__option').on('click', function(){
+    var value =  $(this).attr('data-value');
+    $('#select-type').val(value);
+    $('.select_checked').text(value);
+    $('.select__dropdown').toggleClass('select__dropdown_open')
+  });
+  $('a[href^="#"]').click(function(){
+    var el = $(this).attr('href');
+    $('html, body').animate({scrollTop: $(el).offset().top -120 + 'px'});
+    return false;
+  });
+  $('input[type="tel"]').mask("+7 (999) 999-99-99");
+}); 
